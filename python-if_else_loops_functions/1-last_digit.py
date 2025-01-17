5eddcd59472879fd08ed4c735a9f12b8b44338bc
Franco-byte <fd.reyes3110@gmail.com>
2025-01-17 18:37:35 -0300

    correct output for last digit 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
+'''import random
+number = random.randint(-10000, 10000)'''
+number = 0
 if number < 0:
     number = -number
     last_dig = number % 10
