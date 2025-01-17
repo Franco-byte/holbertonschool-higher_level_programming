b1467bf9c8c388e656b2c6d98ad57c859a274bd6
Franco-byte <fd.reyes3110@gmail.com>
2025-01-17 18:33:10 -0300

    Fix positive valiues

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,8 @@ if number < 0:
     last_dig = number % 10
     last_dig = -last_dig
     number = -number
+else:
+    last_dig = number % 10
 if last_dig > 5:
     print(f"Last digit of {number} is {last_dig} and is greater than 5")
 elif last_dig == 0:
