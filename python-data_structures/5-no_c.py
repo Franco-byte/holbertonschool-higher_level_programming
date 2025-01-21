e267801cdb0ae840329fe2316c82c7e7ca8f15ce
Franco-byte <fd.reyes3110@gmail.com>
2025-01-21 17:04:28 -0300

    Multiple skiped Cc fixed

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     my_list = list(my_string)
+    noc_list = []
     for char in my_list:
-        if char == 'C':
-            my_list.remove('C')
-        if char == 'c':
-            my_list.remove('c')
-    cadena = ''.join(my_list)
+        if char not in ('C', 'c'):
+            noc_list.append(char)
+    cadena = ''.join(noc_list)
     return cadena
