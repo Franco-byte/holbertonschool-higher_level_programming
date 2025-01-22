65708a6bc37ae23c35b634db098da5fa95915839
Franco-byte <fd.reyes3110@gmail.com>
2025-01-22 12:28:53 -0300

    \n error fixed

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print()
         for num in row:
             print("{:d} ".format(num), end='')
-    print()
+            if num is row[-1]:
+                print()
+  
