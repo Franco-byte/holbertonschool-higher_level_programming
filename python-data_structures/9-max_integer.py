d6cc84b488d4225d06f974904f24298757c0abfb
Franco-byte <fd.reyes3110@gmail.com>
2025-01-22 14:47:24 -0300

    Empty list fixed

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    copy = my_list
-    copy.sort(reverse=True)
-    return copy[0] 
+    if len(my_list) == 0:
+        return None
+    else:
+        copy = my_list
+        copy.sort(reverse=True)
+        return copy[0] 
