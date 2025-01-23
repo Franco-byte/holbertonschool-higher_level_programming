8c9881ce97166fdcb6842bf54bf8f45a557e7440
Franco-byte <fd.reyes3110@gmail.com>
2025-01-23 18:31:42 -0300

    exchange between my_list and new_list

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,6 +2,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for num in range(len(new_list)):
-        if my_list[num] == search:
-            my_list[num] = replace
+        if new_list[num] == search:
+            new_list[num] = replace
     return new_list
