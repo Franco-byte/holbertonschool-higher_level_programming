b8649097e3352575327793d3f574742cdd04c25f
Franco-byte <fd.reyes3110@gmail.com>
2025-01-23 20:06:40 -0300

    not comcidence, more than 2 coincidences

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    common = set()
     for elemnt in set_1:
         if elemnt in set_2:
-            return elemnt
-    return 0
+            common.add(elemnt)
+    return common
