fb1b023d5b67e8045b88c73569480c14327c1f5a
Franco-byte <fd.reyes3110@gmail.com>
2025-01-23 18:28:36 -0300

    bu

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for num in range(len(my_list)):
+    for num in range(len(new_list)):
         if my_list[num] == search:
             my_list[num] = replace
     return new_list
