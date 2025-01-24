aa93ddc8b5bdbf01578080826fddd0b8a620f1fb
Franco-byte <fd.reyes3110@gmail.com>
2025-01-24 18:01:00 -0300

    Traductor roman numbers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
+    rom_list = list(roman_string)
+    integer = 0
+
+    for i in range(len(rom_list)):
+        if i + 1 < len(rom_list) and roman[rom_list[i]] < roman[rom_list[i + 1]]:
+            integer -= roman[rom_list[i]]
+        else:
+            integer += roman[rom_list[i]]
+    return integer
