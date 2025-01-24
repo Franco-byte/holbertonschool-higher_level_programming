9b091f764881f07b9e4c74d3e55029e7b6ecab45
Franco-byte <fd.reyes3110@gmail.com>
2025-01-24 18:08:32 -0300

    add isinstance(roman_string, str) for the non strings

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     roman = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
     rom_list = list(roman_string)
