11fc5e0efa4bca88c1a834ebb7d07a5f3d126680
Franco-byte <fd.reyes3110@gmail.com>
2025-01-24 16:51:43 -0300

    Use the map function

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    order_dict = dict(sorted(a_dictionary.items(), key= lambda item: item[1], reverse=True))
+    order_dict = dict(sorted(a_dictionary.items(), key=lambda item: item[1], reverse=True))
     return list(order_dict.keys())[0]
