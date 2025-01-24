e1c0376a44f3ec4aaad6a3c399891feca0fb627b
Franco-byte <fd.reyes3110@gmail.com>
2025-01-24 13:10:46 -0300

    correct output

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1.union(set_2)
+    final_set = set_1.union(set_2)
+    for repeat in set_1:
+        if repeat in set_2:
+            final_set.remove(repeat)
+    return final_set
