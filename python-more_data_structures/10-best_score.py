4cc68b9005ed78a6df887756dd92012429d40980
Franco-byte <fd.reyes3110@gmail.com>
2025-01-24 16:45:16 -0300

    Best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    order_dict = dict(sorted(a_dictionary.items(), key= lambda item: item[1], reverse=True))
+    return list(order_dict.keys())[0]
