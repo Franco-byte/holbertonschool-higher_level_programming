7dabc8be5d6e1529873f5e73184a06c5fd6dcf9e
Franco-byte <fd.reyes3110@gmail.com>
2025-02-06 17:18:29 -0300

    Task 3, commit 1

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Funtion to compare instans and clses
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    Return True if "obj" is a instance of "a_class", otherwise False
+    '''
+    return isinstance(obj, a_class)
