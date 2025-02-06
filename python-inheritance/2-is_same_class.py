f259affc538b60bedfe1b17cd4b34dc463ca7228
Franco-byte <fd.reyes3110@gmail.com>
2025-02-06 17:06:43 -0300

    Task 2, commit 1

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Funtion to compare instans and clses
+'''
+
+def is_same_class(obj, a_class):
+    '''
+    Return True if "obj" is a instance of "a_class", otherwise False
+    '''
+    return type(obj) is a_class
