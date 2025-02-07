7bbe721a6bf117151e9add1c5e5a3c3ea48ab0a0
Franco-byte <fd.reyes3110@gmail.com>
2025-02-07 15:57:42 -0300

    Task 10, commit 1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Square inherated to Rectangle
+'''
+
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    '''
+    Create a Square
+    '''
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
