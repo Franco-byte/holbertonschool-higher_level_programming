c138dcd0a72a07e78386e5db13af63ee23e4989c
Franco-byte <fd.reyes3110@gmail.com>
2025-02-07 14:37:50 -0300

    Task 8, commit 1

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-nase_geometry').BaseGeometry
+'''
+Ineheting
+'''
+
+
+class Rectangle(BaseGeometry):
+    '''
+    Inherits
+    '''
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
