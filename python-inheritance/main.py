1eaa56b633247c8fa437277e0eb0c91fe7add731
Franco-byte <fd.reyes3110@gmail.com>
2025-02-07 15:46:54 -0300

    Task 9, commit 1

--- python-inheritance/main.py
+++ python-inheritance/main.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-Rectangle = __import__('8-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
-bg = Rectangle(22, "3")
-bg = Rectangle(8, 89)
+r = Rectangle(3, 5)
 
-try:
-    bg.integer_validator("name", "John")
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("age", 0)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    bg.integer_validator("distance", -4)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+print(r)
+print(r.area())
