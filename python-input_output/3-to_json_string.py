2218492fdefed5e3dad21bff147676b0251476af
Franco-byte <fd.reyes3110@gmail.com>
2025-02-12 21:00:22 -0300

    Task 3, commit 1

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Learning to use json.dumps() to traslate
+'''
+
+
+import json
+
+def to_json_string(my_obj):
+    '''
+    json.dumps() traslate python to json
+    '''
+    return json.dumps(my_obj)
