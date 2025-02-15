1746f42a1685d1f9b7432645acca6118a1d70df6
Franco-byte <fd.reyes3110@gmail.com>
2025-02-14 22:48:07 -0300

    Task 11, commit 1

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+Create a Student
+'''
+
+
+class Student:
+    '''
+    This is a student :D
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        result = {}
+        for key in attrs:
+            if hasattr(self, key):
+                result[key] = getattr(self, key)
+        return result
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
