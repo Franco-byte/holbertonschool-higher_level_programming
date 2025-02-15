fb9ce8babda5aaa3d68c53ffd839494dbe3bb3c0
Franco-byte <fd.reyes3110@gmail.com>
2025-02-14 23:57:23 -0300

    Task 12, commit 1

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Pascal' triangle
+'''
+
+
+def pascal_triangle(n):
+    '''
+    how to create a pascal'triangle
+    '''
+    triangle = [[1], [1, 1]]
+    prev_row = [1, 1]
+    new_row = [1]
+
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    if n <= 0:
+        return []
+    
+    for row in range(n):
+        new_row = [1]
+        for i in range(len(prev_row) - 1):
+            new_row.append(prev_row[i] + prev_row[i + 1])
+        new_row.append(1)
+        triangle.append(new_row)
+        prev_row = new_row
+    
+    return triangle
