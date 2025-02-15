7374dff94a9d16c4f3b8c3bd880005784a6bddf4
Franco-byte <fd.reyes3110@gmail.com>
2025-02-15 17:46:53 -0300

    enconding= add

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,12 +4,12 @@ import csv
 
 def convert_csv_to_json(filename):
     try:
-        with open(filename, "r", "utf-8") as csv_file:
+        with open(filename, "r", encoding="utf-8") as csv_file:
             reader = csv.DictReader(csv_file)
-            date = list(reader)
+            data = list(reade   r)
 
-        with open("data.json", "w", "utf-8") as j_file:
-            json.dump(date, j_file)
+        with open("data.json", "w", encoding="utf-8") as j_file:
+            json.dump(data, j_file)
             return True
     except Exception:
         return False    
