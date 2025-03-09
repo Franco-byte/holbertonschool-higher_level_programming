fc61a2c6836de258ee3cfb6159d197b4aa2333f6
Franco-byte <fd.reyes3110@gmail.com>
2025-03-09 15:44:20 -0300

    "is not" to "<>"

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Display list by ordern to score
-SELECT score, name FROM second_table WHERE name IS NOT '' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name <> '' ORDER BY score DESC;
