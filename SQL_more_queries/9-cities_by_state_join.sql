be7441857e2e2a837f7c0971bf6f7f2da1c83b78
Franco-byte <fd.reyes3110@gmail.com>
2025-03-13 17:56:35 -0300

    rendido...

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- List al cities in database 
 SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.state_id = states.id
+FROM cities, states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
